565055272fb58617e03973b39ef510caf3d758e1
vikash <vikashraj144@gmail.com>
2019-11-23 23:20:17 +0530

    constant

---
+++ react/src/utils/Constants.js
@@ -0,0 +1,10 @@
+// header config
+export const headerInfo = {
+    headers: { 
+            'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    responseType: 'blob'
+  }
+
+// base url setup remove these and put in environment variable
+export const baseURL = 'http://localhost:8080/';
