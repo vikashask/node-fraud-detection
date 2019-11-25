effad8a96c4c3f7596ebed8495f50c68439b4ebe
vikash <vikashraj144@gmail.com>
2019-11-25 23:38:10 +0530

    routes updated

--- api/routes/order.routes.js
+++ api/routes/order.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const commonMiddleware = require('../middleware/common.middleware');
-var validationError = require('../utils/validationError');
 
 const orderController = require('../controllers/orderController');
+
 router.get('/get-order/:brandId?', commonMiddleware.getBrandIdValidator, orderController.getOrder);
-router.post('/add-order',validationError.slugValidation, commonMiddleware.postBrandIdValidator,orderController.postOrder);
+router.post('/add-order',orderController.postOrder);
 
 module.exports = router;
